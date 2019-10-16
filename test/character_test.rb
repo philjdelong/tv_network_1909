dac33aac74b4f7c6bc548c88ddafbc9f10520f5d
Phil DeLong <philjdelong@gmail.com>
2019-10-16 09:11:28 -0600

    Create character test

---
+++ test/character_test.rb
@@ -0,0 +1,24 @@
+require'./lib/character'
+require'pry'
+
+class CharacterTest < Test::MiniTest
+  def setup
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+  end
+
+  def test_it_exists
+    assert_instance_of Character, @kitt
+  end
+
+  def test_it_has_a_name
+    assert_equal "KITT", @kitt.name
+  end
+
+  def test_it_is_an_actor
+    assert_equal "William Daniels", @kitt.actor
+  end
+
+  def test_it_has_a_salary
+    assert_equal 1000000, @kitt.salary
+  end
+end
